20fdf03e72845d18accc1dbe18b37fadc9c1ce00
Nic McPhee <mcphee@morris.umn.edu>
2022-08-27 16:01:26 -0500

    Add threading to server with Tokio
    This brings in Tokio to make the `main` and `echo_stream` function async and provide the ability to spawn threads to handle incoming connections.

--- echo-server/src/main.rs
+++ echo-server/src/main.rs
@@ -3,41 +3,47 @@
 #![warn(clippy::unwrap_used)]
 #![warn(clippy::expect_used)]
 
-use std::{io::{Error, Read, Write}, net::{TcpListener, TcpStream}};
+use std::io;
+
+use tokio::net::{TcpListener, TcpStream};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 const BUFFER_SIZE: usize = 128;
 
 // Issues to deal with:
 // - Our error handling is fairly weak.
-// - We have no threading, so we only handle one connection at a time.
 // - There should be "proper" logging added to this.
+// - Add something like `clap` to handle command line arguments
+//   - We could specify the port number that way.
 
-fn main() -> Result<(), Error> {
+#[tokio::main]
+async fn main() -> io::Result<()> {
     // TODO: Let the user set this port via the command line.
-    let listener = TcpListener::bind("127.0.0.1:60606")?;
+    // TODO: Handle errors when binding ot the address.
+    let listener = TcpListener::bind("127.0.0.1:60606").await?;
 
-    // accept connections and process them serially
-    for stream in listener.incoming() {
-        // TODO: We might want to log or print something here
-        // if we get an Error type in `stream`.
-        // TODO: We also should handle any error coming back from
-        // `echo_stream`.
-        echo_stream(stream?)?;
+    loop {
+        // TODO: Handle errors when accepting requests.
+        let (socket, _) = listener.accept().await?;
+        tokio::spawn(async move {
+            // TODO: Handle error when processing socket.
+            echo_stream(socket).await.expect("There was a problem handling a socket");
+        });
     }
-    Ok(())
 }
 
-fn echo_stream(mut stream: TcpStream) -> Result<(), Error> {
-    println!("Handling a stream: {:?}", stream);
+async fn echo_stream(mut socket: TcpStream) -> io::Result<()> {
+    println!("Handling a stream: {:?}", socket);
     let mut buf = [0; BUFFER_SIZE];
     // TODO: Handle the error case here.
     loop {
-        let num_read_bytes = stream.read(&mut buf)?;
+        // Handle error when reading from socket.
+        let num_read_bytes = socket.read(&mut buf).await?;
         if num_read_bytes == 0 {
-            println!("Done handling stream {:?}", stream);
+            println!("Done handling stream {:?}", socket);
             return Ok(());
         }
         // TODO: Handle the error case here.
-        stream.write_all(&buf[0..num_read_bytes])?;
+        socket.write_all(&buf[0..num_read_bytes]).await?;
     }
 }
