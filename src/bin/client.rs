568780d5a689b1415348ab65017be8508892d766
Nic McPhee <mcphee@morris.umn.edu>
2022-09-17 15:26:50 -0500

    Use `clap` to parse client arguments
    This adds the `clap` command-line argument parser and sets things up to allow the user to specify either of the IP address or port number, using localhost and 60606 as defaults.

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,12 +1,28 @@
-use std::io::{self, Read, Write};
+use std::{io::{self, Read, Write}, net::{IpAddr, SocketAddr}};
+
+use clap::Parser;
 
 use tokio::{net::TcpStream, io::{AsyncWriteExt, AsyncReadExt}};
 
 const BUFFER_SIZE: usize = 128;
 
+/// Simple echo client
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+   /// The IP address to connect to
+   #[clap(short, long, value_parser, default_value = "127.0.0.1")]
+   ip_address: IpAddr,
+
+   /// The port number to connect to
+   #[clap(short, long, value_parser, default_value_t = 60606)]
+   port: u16,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), io::Error> {
-    let server_address = "127.0.0.1:60606";
+    let args = Args::parse();
+    let server_address = SocketAddr::new(args.ip_address, args.port);
     let mut socket = TcpStream::connect(server_address).await?;
     let mut buf = [0; BUFFER_SIZE];
 
