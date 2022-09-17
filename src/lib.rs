066a43197b2f90a286128ee71ca7bdeb9703c4b6
Nic McPhee <mcphee@morris.umn.edu>
2022-09-17 16:43:35 -0500

    Change the default port
    It turns out that the echo protocol (https://en.wikipedia.org/wiki/Echo_Protocol) runs on port 7 historically. I was going to change to that port, but non-root users can't listen on ports less than 1,000 on most systems.
    
    I then poked around with some other ports that involved mostly 7's, and found that 7707 isn't being use by much, so changed the default to that.

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ use std::net::IpAddr;
 use clap::Parser;
 
 pub const BUFFER_SIZE: usize = 128;
+pub const DEFAULT_PORT: u16 = 7707;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -12,6 +13,6 @@ pub struct Args {
    pub ip_address: IpAddr,
 
    /// The port number to connect to
-   #[clap(short, long, value_parser, default_value_t = 60606)]
+   #[clap(short, long, value_parser, default_value_t = DEFAULT_PORT)]
    pub port: u16,
 }
